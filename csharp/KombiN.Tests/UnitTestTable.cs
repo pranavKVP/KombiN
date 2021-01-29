60bd2e4c8553c9835056c5df2a81df3366abb146
Pranavkumar Patel <code@pranav.ninja>
2021-01-29 13:55:38 -0600

    test files

---
+++ csharp/KombiN.Tests/UnitTestTable.cs
@@ -0,0 +1,58 @@
+
+using System;
+using Xunit;
+
+[assembly: CLSCompliant(true)]
+namespace KombiN.Tests
+{
+    public class UnitTestTable
+    {
+        [Fact]
+        public void TestAll60False()
+        {
+            bool result = true;
+            for (int X = 1; X <= 60; X++)
+            {
+                for (int Y = 1; Y <= 60; Y++)
+                {
+                    Table myXY = new Table(X, Y, false);
+                    for (long i = 1; i <= X * Y; i++)
+                    {
+                        long ai, bi;
+                        (ai, bi) = myXY.GetElementsAtIndex(i);
+                        long index = myXY.GetIndexOfElements(ai, bi);
+                        if (i != index)
+                        {
+                            result = false;
+                        }
+                    }
+                }
+            }
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TestAll60True()
+        {
+            bool result = true;
+            for (int X = 1; X <= 60; X++)
+            {
+                for (int Y = 1; Y <= 60; Y++)
+                {
+                    Table myXY = new Table(X, Y, true);
+                    for (long i = 0; i <= (X * Y) - 1; i++)
+                    {
+                        long ai, bi;
+                        (ai, bi) = myXY.GetElementsAtIndex(i);
+                        long index = myXY.GetIndexOfElements(ai, bi);
+                        if (i != index)
+                        {
+                            result = false;
+                        }
+                    }
+                }
+            }
+            Assert.True(result);
+        }
+    }
+}
