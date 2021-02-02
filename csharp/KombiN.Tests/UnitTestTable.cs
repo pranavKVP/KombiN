5cd76730cbd1ce6a24ad8539820493983b270c21
Pranavkumar Patel <code@pranav.ninja>
2021-02-02 12:01:39 -0600

    remove new line before opening brace

--- csharp/KombiN.Tests/UnitTestTable.cs
+++ csharp/KombiN.Tests/UnitTestTable.cs
@@ -3,26 +3,19 @@ using System;
 using Xunit;
 
 [assembly: CLSCompliant(true)]
-namespace KombiN.Tests
-{
-    public class UnitTestTable
-    {
+namespace KombiN.Tests {
+    public class UnitTestTable {
         [Fact]
-        public void TestAll60False()
-        {
+        public void TestAll60False() {
             bool result = true;
-            for (int X = 1; X <= 60; X++)
-            {
-                for (int Y = 1; Y <= 60; Y++)
-                {
+            for (int X = 1; X <= 60; X++) {
+                for (int Y = 1; Y <= 60; Y++) {
                     Table myXY = new Table(X, Y, false);
-                    for (long i = 1; i <= X * Y; i++)
-                    {
+                    for (long i = 1; i <= X * Y; i++) {
                         long ai, bi;
                         (ai, bi) = myXY.GetElementsAtIndex(i);
                         long index = myXY.GetIndexOfElements(ai, bi);
-                        if (i != index)
-                        {
+                        if (i != index) {
                             result = false;
                         }
                     }
@@ -32,21 +25,16 @@ namespace KombiN.Tests
         }
 
         [Fact]
-        public void TestAll60True()
-        {
+        public void TestAll60True() {
             bool result = true;
-            for (int X = 1; X <= 60; X++)
-            {
-                for (int Y = 1; Y <= 60; Y++)
-                {
+            for (int X = 1; X <= 60; X++) {
+                for (int Y = 1; Y <= 60; Y++) {
                     Table myXY = new Table(X, Y, true);
-                    for (long i = 0; i <= (X * Y) - 1; i++)
-                    {
+                    for (long i = 0; i <= (X * Y) - 1; i++) {
                         long ai, bi;
                         (ai, bi) = myXY.GetElementsAtIndex(i);
                         long index = myXY.GetIndexOfElements(ai, bi);
-                        if (i != index)
-                        {
+                        if (i != index) {
                             result = false;
                         }
                     }
