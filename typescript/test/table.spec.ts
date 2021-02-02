af4e99d5f7195dde22cd1a5a38c21d7bcbb880a1
Pranavkumar Patel <code@pranav.ninja>
2021-02-02 12:12:50 -0600

    test file

---
+++ typescript/test/table.spec.ts
@@ -0,0 +1,41 @@
+import { Table } from '../src/index';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Table class - test all 100 nonZero', () => {
+    it('should return true', () => {
+        let result = true;
+        for (let X = 1; X <= 100; X++) {
+            for (let Y = 1; Y <= 100; Y++) {
+                const myXY: Table = new Table(X, Y, false);
+                for (let i = 1; i <= X * Y; i++) {
+                    const [ai, bi] = myXY.GetElementsAtIndex(i);
+                    const index = myXY.GetIndexOfElements(ai, bi);
+                    if (i != index) {
+                        result = false;
+                    }
+                }
+            }
+        }
+        expect(result).to.equal(true);
+    });
+});
+
+describe('Table class - test all 100 zero', () => {
+    it('should return true', () => {
+        let result = true;
+        for (let X = 1; X <= 100; X++) {
+            for (let Y = 1; Y <= 100; Y++) {
+                const myXY: Table = new Table(X, Y, true);
+                for (let i = 0; i <= X * Y - 1; i++) {
+                    const [ai, bi] = myXY.GetElementsAtIndex(i);
+                    const index = myXY.GetIndexOfElements(ai, bi);
+                    if (i != index) {
+                        result = false;
+                    }
+                }
+            }
+        }
+        expect(result).to.equal(true);
+    });
+});
